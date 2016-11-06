48869bd9e7c1cebcb2fb8c092c938804dbed42af
laura.abukauskaite@visma.com <laura.abukauskaite@visma.com>
2016-11-06 21:04:56 +0200

    -Average Number Calcularot

---
+++ AverageCalculator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įveskite sąrašo ilgį:");
+            string ArrayLengthString = Console.ReadLine();
+            int ArrayLength;
+            if (!int.TryParse(ArrayLengthString, out ArrayLength))
+            {
+                Console.WriteLine("Neteisingai ivesti parametrai");
+                return;
+            }
+            if (ArrayLength <= 0)
+            {
+                Console.WriteLine("Saraso ilgis turi buti didesnis uz 0");
+                return;
+            }
+            Console.WriteLine("Iveskite " + ArrayLength + " skaicius");
+            decimal sum = 0;
+            for (int i = 0; i < ArrayLength; i++)
+            {
+                start:
+                Decimal Number;
+                string NumberString = Console.ReadLine();
+                if (!decimal.TryParse(NumberString, out Number))
+                {
+                    Console.WriteLine("Iveskite skaiciu");
+                    goto start;
+                }
+                sum += Number;
+            }
+            Console.WriteLine("Atsakymas: " + sum / ArrayLength);
+            Console.ReadLine();
+        }
+    }
+}
+
